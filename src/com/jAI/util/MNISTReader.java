664006a49a6f5725e4c6d226d7ebcb3f7a16909c
Itay Bachar <itay.bachar01@gmail.com>
2019-02-28 22:30:26 -0500

    Added mnist reader, didnt check if it works, training it is slow and doesnt work...

--- src/com/jAI/util/MNISTReader.java
+++ src/com/jAI/util/MNISTReader.java
@@ -1,18 +1,82 @@
 package com.jAI.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class MNISTReader {
+    private static FileInputStream inputStream;
 
-    public static double[][] loadImages(File imageFile){
-        double[][] images = new double[3][3];
+    public static double[][] loadImages(File imageFile) {
+        try {
+            inputStream = new FileInputStream(imageFile);
 
-        return images;
+            //Skip Magic number
+            inputStream.skip(4);
+
+            //Read Image Number
+            int imageNum = nextNByte(4);
+
+            //Get Image dimensions
+            int rows = nextNByte(4);
+            int cols = nextNByte(4);
+
+            //Initialize the image array
+            double[][] images = new double[imageNum][rows*cols];
+
+            //Place the input
+            for(int i = 0; i<imageNum;i++){
+                for(int k = 0; k<cols*rows;k++){
+                    images[i][k]= nextNByte(1);
+                }
+            }
+
+            //Close Input Stream
+            inputStream.close();
+
+            return images;
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+        }
+        return null;
     }
 
-    public static double[][] loadLabels(File labelFile){
-        double[][] labels = new double[3][3];
+    public static double[][] loadLabels(File labelFile) {
+        try {
+            inputStream = new FileInputStream(labelFile);
 
-        return labels;
+            //Skip Magic number
+            inputStream.skip(4);
+
+            //Read Image Number
+            int labelNum = nextNByte(4);
+
+            //Initialize the image array
+            double[][] labels = new double[labelNum][10];
+
+            //Place the input
+            for(int i = 0; i<labelNum;i++){
+                int label = nextNByte(1);
+                labels[i][label] = 1;
+            }
+
+            //Close Input Stream
+            inputStream.close();
+
+            return labels;
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+        }
+        return null;
     }
+
+    private static int nextNByte(int n) throws IOException {
+        int k=inputStream.read()<<((n-1)*8);
+        for(int i =n-2;i>=0;i--){
+            k+=inputStream.read()<<(i*8);
+        }
+        return k;
+    }
+
+
 }
--- src/test.java
+++ src/test.java
@@ -1,24 +1,27 @@
 import com.jAI.ANN;
 import com.jAI.Activations;
 import com.jAI.util.MNISTReader;
+import com.jAI.util.Matrix;
+import com.jAI.util.TrainingSet;
 
 import java.io.File;
 
 public class test {
-    public static void main(String[] args) {
-       double[][] data = {
-               {1,1},
-               {1,0},
-               {0,1},
-               {0,0}
-       };
 
-       double[][] out = {{0},{1},{1},{0}};
+    public static void annTest() {
+        double[][] data = {
+                {1, 1},
+                {1, 0},
+                {0, 1},
+                {0, 0}
+        };
 
-       ANN brain = new ANN(2,new int[]{5},1,0.1,Activations::sigmoid,Activations::sigmoidPrime);
+        double[][] out = {{0}, {1}, {1}, {0}};
 
-       for(int i = 0; i<20000;i++)
-       brain.mini_batch(data,out,1,false);
+        ANN brain = new ANN(2, new int[]{5}, 1, 0.1, Activations::sigmoid, Activations::sigmoidPrime);
+
+        for (int i = 0; i < 20000; i++)
+            brain.mini_batch(data, out, 1, false);
 
 //       for(int i = 0; i<100000;i++){
 //           int index = (int)(Math.random()*4);
@@ -37,5 +40,37 @@ public class test {
 
         System.out.println("(0,0)=>0");
         brain.feedForward(data[3]).print();
+    }
+
+    public static void mnistTest() {
+        File imgFile = new File("/home/itay/Desktop/HandwrittenDigits/train-images.idx3-ubyte");
+        File labelFile = new File("/home/itay/Desktop/HandwrittenDigits/train-labels.idx1-ubyte");
+        double[][] img = MNISTReader.loadImages(imgFile);
+        double[][] label = MNISTReader.loadLabels(labelFile);
+
+        TrainingSet t = new TrainingSet(img,label);
+        ANN brain = new ANN(img[0].length, new int[]{15}, label[0].length, 0.1, Activations::sigmoid, Activations::sigmoidPrime);
+        for(int j = 0;j<10;j++) {
+//            t.shuffle();
+//            for (int i = 0; i < t.getSize(); i++) {
+//                TrainingSet.Data data = t.data.get(i);
+//                brain.feedForward(data.input);
+//                brain.SGD(data.output);
+//            }
+            brain.mini_batch(img,label,20,false);
+        }
+
+        TrainingSet.Data t2 = t.pickRandom();
+
+        System.out.println("test");
+        brain.feedForward(t2.input).print();
+
+        System.out.println("desire");
+        Matrix.fromArray(new double[][]{t2.output}).print();
+
+    }
+
+    public static void main(String[] args) {
+        mnistTest();
     }
 }
